c4c69dfa089f92c72d110c29d5f89c55c11ba7b4
Steven <deletestvn@gmail.com>
2022-01-13 11:12:32 -0500

    add Nesting java solution

---
+++ Java/7_3_Nesting.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(String S) {
+        // write your code in Java SE 8
+        Stack<Character> stack = new Stack<Character>();
+
+        for(int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            if(stack.empty() || c == '(') stack.push(c);
+            else if(c == ')' && stack.peek() != '(') return 0;
+            else stack.pop();
+        }
+
+        return stack.empty() ? 1 : 0; 
+    }
+}
